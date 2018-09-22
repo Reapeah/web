72cb2ed13131372cf43fc13e01667cf6b093a444
Reapeah <40251850+Reapeah@users.noreply.github.com>
2018-09-23 00:29:42 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import random
+import os
 
 app = Flask(__name__)
 @app.route('/random',methods=['POST'])
@@ -17,5 +18,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    
 
