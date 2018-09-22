e5ce719dfe8d52c31c0fa324e8787ebb10a03359
Reapeah <wildstyle3@yahoo.com>
2018-09-22 22:23:27 +0200

    'message'

---
+++ weather.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+import random
+
+app = Flask(__name__)
+@app.route('/random',methods=['POST'])
+def random_input():
+    city = request.form['city']
+    words = city.split()
+    if len(words)==0:
+        return render_template('error.html',x="Try again with actual input")
+    x = random.randint(0,len(words)-1)
+    return render_template('temperature.html',x=words[x])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
