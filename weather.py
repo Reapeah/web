b7e3552423e6dd4e2d7d0bd451a9c7f145fc7d65
Reapeah <40251850+Reapeah@users.noreply.github.com>
2018-09-23 22:27:30 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, request
 import requests
 import random
+import json
+
+app_id = 'b552ef6b'
+app_key = '0ef5a2dd0ef6b363bd6a9830139a0c2c'
 
 app = Flask(__name__)
 @app.route('/random',methods=['POST'])
 def random_input():
-    city = request.form['city']
-    words = city.split()
-    if len(words)==0:
-        return render_template('error.html',x="Try again with actual input")
-    if words[0] == "gnome" or words[0] == "gnomed":
-        return render_template('gnome.html')
-    x = random.randint(0,len(words)-1)
-    return render_template('temperature.html',x=words[x])
+    language = 'en'
+    word_id =request.form['city']
 
-
+    try:   
+        url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
+        r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+        a = (r.json()['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'])
+        b = str(a)
+        return render_template('error.html',x = b[2:-2:])
+    except:
+        return render_template('error.html',x = "That word does not exist in the Oxford dictionairy :(")
 
 @app.route('/')
 def index():
